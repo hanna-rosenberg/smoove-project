5c7723eb0058b8ccd817f754e82fa7a970d60047
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2021-11-30 18:22:11 +0100

    this version works too

--- java.js
+++ java.js
@@ -24,16 +24,10 @@ slidefun(counter);
 function plusSlides(n) {
   counter += n;
   slidefun(counter);
-  resetTimer();
 }
 function currentSlide(n) {
   counter = n;
   slidefun(counter);
-  resetTimer();
-}
-function resetTimer() {
-  clearInterval(timer);
-  timer = setInterval(autoSlide, 8000);
 }
 
 function slidefun(n) {
